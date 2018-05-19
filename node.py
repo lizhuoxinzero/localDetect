6410a3885674461c71394b9da6b788968fe6c72c
1661023471@qq.com <1661023471@qq.com>
2018-05-19 03:51:49 +0000

    add time log

--- node.py
+++ node.py
@@ -1,16 +1,19 @@
 import socket
 import netifaces
 import json
+import os
+import getpass
+
+username = getpass.getuser()
 
 address =('', 38080)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR|socket.SO_BROADCAST,1)
 s.bind(address)
-
 while True:
     data, addr = s.recvfrom(2048)
     if not data:
-        print "client has exist"
+        print "[%s]client has exist"%time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         break
     if data == "IS RASPBERRY!":
         print "received:", data, "from", addr
@@ -22,8 +25,8 @@ while True:
             if netifaces.AF_INET not in info:
                 continue
             results.append(info[netifaces.AF_INET][0]['addr'])
-        msg = "%s %s"%(hostname, json.dumps(results))
-        print(msg)
+        msg = "%s %s %s"%(hostname, username, json.dumps(results))
+        print "[%s]%s"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),msg)
         s.sendto(msg, addr)
 
 s.close()
